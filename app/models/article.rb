61549889d8f853cf5ec25856a2f43151fd5cf970
Dennis Guardado <dennisbusiness2016@gmail.com>
2017-11-15 16:53:01 -0600

    add home and config routes

---
+++ app/models/article.rb
@@ -0,0 +1,15 @@
+class Article < ApplicationRecord
+	
+end
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :integer          not null, primary key
+#  title        :string
+#  body         :text
+#  visits_count :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
